b563b414f4a344fa9f0c4f1650a228ec5c10f24c
aathi-immortal <89212658+aathi-immortal@users.noreply.github.com>
2023-07-29 10:02:49 +0530

    Delete quizMicroService/quizservice directory

--- quizMicroService/quizservice/src/main/java/com/quiz/quizservice/entity/QuizDto.java
+++
@@ -1,7 +0,0 @@
-package com.quiz.quizservice.entity;
-
-public class QuizDto {
-   public String category;
-    public int numberOfQuestion;
-    public String title;
-}
--- quizMicroService/quizservice/src/main/java/com/quiz/quizservice/repository/QuizRepository.java
+++
@@ -1,9 +0,0 @@
-package com.quiz.quizservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.quiz.quizservice.entity.Quiz;
-
-public interface QuizRepository extends JpaRepository<Quiz, Integer> {
-
-}
